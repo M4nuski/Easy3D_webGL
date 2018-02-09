4f389b557e2a87d073ee2cfa47705e7551219f1c
Kaven Gagne <kaven.gagne@gmail.com>
2018-02-08 19:08:39 -0500

    ajoute default main.js based on Quickstart de electron.org

---
+++ main.js
@@ -0,0 +1,36 @@
+
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+
+let window;
+
+function createMainWindow() {
+    window = new BrowserWindow({ width: 1280, height: 960 });
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, "ver4.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+
+    //window.webContents.openDevTools();
+
+    window.on("close", () => {
+        window = null;
+    });
+}
+
+app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+});
+
+app.on("activate", () => {
+    if (window === null) {
+        createMainWindow();
+    }
+});
