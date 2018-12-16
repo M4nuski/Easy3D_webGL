523344ace3f1eeeffc918c792f10a1945515cf28
M4nuski <the_man994@hotmail.com>
2018-12-16 18:54:03 -0500

    Folderization

---
+++ ver4/pointerLock.js
@@ -0,0 +1,84 @@
+"use strict"
+// https://www.html5rocks.com/en/tutorials/pointerlock/intro/
+
+
+var pLockSupported = 'pointerLockElement' in document ||  'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+var pLockMoveEvent; // callback for captured pointer movements
+var pLockRequested = false;
+var pLockElement; // element that captured the pointer
+const _pLockJitterLimit = 100; // max delta per event to avoid warp-around when browser place the cursor back into the center after exiting the client area
+
+function pLockRequest(element) {
+    pLockElement = element;    
+    if (pLockSupported) {
+        element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock; 
+        pLockRequested = true;
+        element.requestPointerLock(); 
+    } 
+}
+
+function pLockActive() {
+    let plelm = document.pointerLockElement || document.mozPointerLockElement || document.webkitPointerLockElement;
+    return (pLockElement != undefined ) && (plelm == pLockElement);
+}
+
+function pLockExit() {
+    document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock || document.webkitExitPointerLock;
+    pLockRequested = false;
+    //pLockElement = undefined;
+    document.exitPointerLock();
+}
+
+function pLockToggle(element) {
+  // pLockElement = element;
+   console.log("active " + pLockActive());
+   console.log("pre requested " + pLockRequested);
+    pLockRequested = !pLockActive();
+    if (pLockActive()) pLockExit();
+    if (pLockRequested) {
+        pLockRequest(element);
+    } else {
+        pLockExit();
+    }
+}
+
+document.addEventListener('pointerlockchange', pLockChangeCallback, false);
+document.addEventListener('mozpointerlockchange', pLockChangeCallback, false);
+document.addEventListener('webkitpointerlockchange', pLockChangeCallback, false);
+
+document.addEventListener('pointerlockerror', pLockErrorCallback, false);
+document.addEventListener('mozpointerlockerror', pLockErrorCallback, false);
+document.addEventListener('webkitpointerlockerror', pLockErrorCallback, false);
+
+function pLockChangeCallback(event) {
+    if (pLockActive()) {
+            // Lock successful, add event
+            pLockElement.addEventListener("mousemove", pLockInternalCallback, false);
+    } else {
+
+            // Lock failed, reset event
+            pLockElement.removeEventListener("mousemove", pLockInternalCallback, false);
+            pLockElement = undefined;
+           // pLockExit();
+    }
+}
+
+function pLockErrorCallback(event) {
+    pLockElement.removeEventListener("mousemove", pLockInternalCallback, false);
+    pLockElement = undefined;
+    console.log("pointerLock Error");
+    //pLockExit();
+}
+
+function pLockInternalCallback(event) {
+    var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+    var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+    if (pLockMoveEvent) {
+        if ((movementX < _pLockJitterLimit) && (movementX > -_pLockJitterLimit) && 
+            (movementY < _pLockJitterLimit) && (movementY > -_pLockJitterLimit)) {
+            pLockMoveEvent(movementX, movementY);
+        }
+    }
+}
+
+
---
+++ ver4/slickHover.js
@@ -0,0 +1,54 @@
+"use strict"
+
+var hover2elements;
+const _hoverclassMarker = ".hover2";
+const _hoverclassSuffixOn = "_hover2on";
+const _hoverclassSuffixOff = "_hover2off";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    hover2elements = document.querySelectorAll(_hoverclassMarker);
+
+    for (var i = 0; i < hover2elements.length; ++i) {
+        hover2elements[i].addEventListener("touchstart", hover2onTouch);
+        hover2elements[i].addEventListener("touchcancel",hover2onTouchCancel);
+        hover2elements[i].addEventListener("touchend",hover2onTouchCancel);
+        hover2elements[i].addEventListener("mouseenter", hover2onMouseEnter);
+        hover2elements[i].addEventListener("mouseleave", hover2OnMouseLeave);
+        hover2elements[i].classList.add(hover2elements[i].id + _hoverclassSuffixOff);
+    }
+});
+
+
+function hover2CollapseAll(){
+    for (var i = 0; i < hover2elements.length; ++i) {
+        hover2elements[i].classList.replace( hover2elements[i].id + _hoverclassSuffixOn, hover2elements[i].id + _hoverclassSuffixOff);
+    }
+}
+
+
+function hover2DisableAll(){
+    //remove hover class from all elements
+    //remove event handlers
+}
+function hover2Disable(elem){
+    //find elem in list, remove events
+    //remove elem from list
+}
+
+function hover2onTouch(event){
+    event.target.classList.replace( event.target.id + _hoverclassSuffixOff, event.target.id + _hoverclassSuffixOn);
+    console.log("touch start");
+}
+function hover2onTouchCancel(event){
+    event.target.classList.replace( event.target.id + _hoverclassSuffixOn, event.target.id + _hoverclassSuffixOff);
+    console.log("touch cancel");
+}
+function hover2onMouseEnter(event){
+    event.target.classList.replace( event.target.id + _hoverclassSuffixOff, event.target.id + _hoverclassSuffixOn);
+    console.log("mouse enter");
+}
+function hover2OnMouseLeave(event){
+    event.target.classList.replace( event.target.id + _hoverclassSuffixOn, event.target.id + _hoverclassSuffixOff);
+    console.log("mouse leave");
+}
