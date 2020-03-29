d4a8238f610e012a89442d77d687c473028fc9e4
M4nuski <the_man994@hotmail.com>
2020-03-29 14:54:54 -0400

    fixed out of order script loading

--- ver5/E3D_Engine.js
+++ ver5/E3D_Engine.js
@@ -1,5 +1,4 @@
 var E3D_scriptList = [
-
     "ver5/math.js",
     "ver5/utility.js",
     "ver5/debug.js",
@@ -21,17 +20,31 @@ var E3D_scriptList = [
 ];
     
 var E3D_numScriptLoaded = 0;
+var E3D_currentScript = "";
+
 function E3D_scriptLoaded() {
     E3D_numScriptLoaded++;
+    // if(E3D_DEBUG_VERBOSE) console.log(E3D_currentScript + " Loaded");
     if (E3D_numScriptLoaded >= E3D_scriptList.length) {
-        log("ScriptsLoaded");
+        console.log("All Scripts Loaded");
         if (E3D_userInit) E3D_userInit();
+    } else E3D_loadNextScript();
+}
+
+function E3D_scriptLoadError(event){
+    console.log("Script Load Error for " + E3D_currentScript + ": " + event.type);
+}
+
+function E3D_loadNextScript() {
+    if (E3D_numScriptLoaded < E3D_scriptList.length) {
+        var s = document.createElement("script");
+        s.type = "text/javascript"; 
+        s.onload = E3D_scriptLoaded;
+        s.onerror = E3D_scriptLoadError;
+        s.src = E3D_scriptList[E3D_numScriptLoaded];
+        E3D_currentScript = E3D_scriptList[E3D_numScriptLoaded];
+        document.head.appendChild(s);
     }
 }
-for (var i = 0; i < E3D_scriptList.length; ++i) {
-    var s = document.createElement("script");
-    s.type = "text/javascript"; 
-    s.onload = E3D_scriptLoaded;
-    s.src = E3D_scriptList[i];
-    document.head.appendChild(s);
-}
+
+E3D_loadNextScript();
